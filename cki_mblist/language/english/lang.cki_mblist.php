d86aacad4444114435c6eaf443a0a2a99e393378
Matt Johnson <matt@packettide.com>
2019-03-12 13:55:02 -0400

    PSR-2

--- cki_mblist/language/english/lang.cki_mblist.php
+++ cki_mblist/language/english/lang.cki_mblist.php
@@ -1,9 +1,8 @@
 <?php
 
 $lang = array(
+    'group_ids_label' => 'Limit list to group(s):',
+    'group_ids_label_notes' => 'If no groups are selected, all members will be available.',
 
-	'group_ids_label' => 'Limit list to group(s):',
-	'group_ids_label_notes' => 'If no groups are selected, all members will be available.',
-
-	''=>''
+    ''=>''
 );
